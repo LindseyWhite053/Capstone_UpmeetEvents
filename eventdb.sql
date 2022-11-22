8b3da47a7f399c18118800e65706d58793f419bd
Lindsey White <LindseyWhite@RocketMortgage.com>
2022-11-22 17:01:20 -0500

    Added Routing and Favorite Page

--- eventdb.sql
+++ eventdb.sql
@@ -15,8 +15,24 @@ create table events (
     PRIMARY KEY (id)
 );
 
-insert into events (name, location, date, time, category, favorite) values ('Lorna shore', 'House of Blues', 'Nov 17 2022', '7:00pm', 'Music', false); 
-insert into events (name, location, date, time, category, favorite) values ('LAKEYAH', 'Magic Stick', 'Nov 17 2022', '7:00pm', 'Music', false);
-insert into events (name, location, date, time, category, favorite) values ('Guster', 'St. Andrews Hall', 'Nov 18 2022', '7:00pm', 'Music', false);
-insert into events (name, location, date, time, category, favorite) values ('EMO PROM', 'The Magic Bag', 'Nov 19 2022', '8:00pm', 'Music', false);
-insert into events (name, location, date, time, category, favorite) values ('Streetlight Manifesto', 'Angora Theatre', 'Nov 20 2022', '7:00pm', 'Music', false);
+create table favorite (
+	id int NOT NULL AUTO_INCREMENT,
+    event_id int,
+    name varchar(50),
+    location varchar(50),
+    date varchar(15),
+    time varchar(6),
+    category varchar(15),
+    favorite boolean,
+    PRIMARY KEY (id)
+);
+
+insert into events (name, location, date, time, category, favorite) values ('Lorna shore', 'House of Blues', 'Nov 17, 2022', '7:00pm', 'Music', false); 
+insert into events (name, location, date, time, category, favorite) values ('LAKEYAH', 'Magic Stick', 'Nov 17, 2022', '7:00pm', 'Music', false);
+insert into events (name, location, date, time, category, favorite) values ('Guster', 'St. Andrews Hall', 'Nov 18, 2022', '7:00pm', 'Music', false);
+insert into events (name, location, date, time, category, favorite) values ('EMO PROM', 'The Magic Bag', 'Nov 19, 2022', '8:00pm', 'Music', false);
+insert into events (name, location, date, time, category, favorite) values ('Streetlight Manifesto', 'Angora Theatre', 'Nov 20, 2022', '7:00pm', 'Music', false);
+
+select f.id, f.event_id, e.name, e.location, e.date, e.time, e.category
+from events e
+join favorite f on event.id - favorites.event_id;
